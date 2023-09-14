b3bcf5633b7af22a7f43e9e144b3a750355bb637
LucasPerez001 <lukassperess19@gmail.com>
2023-09-14 13:07:40 -0300

    trabalho entregue

---
+++ heroi.js
@@ -0,0 +1,12 @@
+//classificador de nivel de Herói
+let nomeHeroi = "Death,the destroyer of wolrds"
+
+for (let xp = 1; xp <=1000; xp ++){ console.log ("O herói " + nomeHeroi +" está no nível Ferro." )}
+for (let xp = 1001; xp<=2000; xp++){ console.log ("O herói " + nomeHeroi +" está no nível Bronze." )}
+for (let xp = 2001; xp<=5000; xp++){ console.log ("O herói " + nomeHeroi +" está no nível Prata." )}
+for (let xp = 6001; xp<=7000; xp++){ console.log ("O herói " + nomeHeroi +" está no nível Ouro." )}
+for (let xp = 7001; xp<=8000; xp++){ console.log ("O herói " + nomeHeroi +" está no nível Platina." )}
+for (let xp = 8001; xp<=9000; xp++){ console.log ("O herói " + nomeHeroi +" está no nível Ascendente." )}
+for (let xp = 9001; xp<=1000; xp++){ console.log ("O herói " + nomeHeroi +" está no nível Imortal." )}
+for (let xp = 1000; xp<=10001; xp++){ console.log ("O herói " + nomeHeroi +" está no nível Radiante." )}
+console.log("Parabéns, o herói " + nomeHeroi + " atingiu o Limite de Experiência do jogo!!!")
